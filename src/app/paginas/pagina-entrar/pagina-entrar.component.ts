c79da6a810fc81e09335153777f09188713bd786
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-01-26 17:08:57 -0300

    fazendo o header

--- src/app/paginas/pagina-entrar/pagina-entrar.component.ts
+++ src/app/paginas/pagina-entrar/pagina-entrar.component.ts
@@ -3,15 +3,19 @@ import { Router } from '@angular/router';
 import { BarraSuperiorComponent } from "../../componentes/barra-superior/barra-superior.component";
 
 @Component({
-    selector: 'app-pagina-entrar',
-    standalone: true,
-    templateUrl: './pagina-entrar.component.html',
-    styleUrl: './pagina-entrar.component.css',
-    imports: [BarraSuperiorComponent]
+  selector: 'app-pagina-entrar',
+  standalone: true,
+  templateUrl: './pagina-entrar.component.html',
+  styleUrl: './pagina-entrar.component.css',
+  imports: [BarraSuperiorComponent]
 })
 export class PaginaEntrarComponent {
   mostrarErroEmail = false
   mostrarErroSenha = false
 
-  constructor(private route: Router) {}
+  constructor(private route: Router) { }
+
+  aoClicarEntrar() {
+    this.route.navigate(["area-do-consumidor"])
+  }
 }
